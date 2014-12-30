60497795da37c4abe6d63cef1638d0faddd5db9c
Andrew <andyjacuw@gmail.com>
2014-12-29 23:06:02 -0800

    Move all files into root directory

---
+++ public/javascripts/app.js
@@ -0,0 +1,45 @@
+(function() {
+  var app = angular.module('account', []);
+
+  app.controller('AccountController', ['$scope', function($scope) {
+    $scope.users = userList;
+
+    $scope.addUser = function(user) {
+      $scope.addingUser = false;
+      $scope.users.push(user);
+      $scope.user = {};
+    };
+
+    $scope.removeUser = function(user) {
+      var index = $scope.users.indexOf(user);
+      $scope.users.splice(index, 1);
+    };
+  }]);
+
+  app.directive('editUser', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'edit_user.html'
+    };
+  });
+
+  app.directive('addUser', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'add_user.html'
+    };
+  });
+
+  var userList = [
+    {
+      firstName: 'Guy',
+      lastName: 'Person',
+      email: 'dude@domain.com'
+    },
+    {
+      firstName: 'Andrew',
+      lastName: 'Jacobson',
+      email: 'andrew@example.com'
+    }
+  ];
+})();
